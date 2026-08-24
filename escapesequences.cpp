9367950e8e516d5dbcfc7ce902ccde001b7c43c2
Abhi-ASTA <abhiraj2009lava@gmail.com>
2026-08-24 13:16:47 +0530

    Add files via upload

---
+++ escapesequences.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+cout << "Neweline: Hello\\nWorld" << endl;
+cout << "Tab: Hello\\tWorld" << endl;
+cout << "Backslash: C:\\\\Programs" << endl;
+cout << "Double Quote: \"Hello\"" << endl;
+cout << "Single Quote: '\''" << endl;
+cout << "Alert: \\a" << endl;
+
+return 0;
+}
