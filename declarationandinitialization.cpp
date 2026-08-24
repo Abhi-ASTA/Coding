7ced5e86a7c15fa1a800cea8dfeea1fba608de46
Abhi-ASTA <abhiraj2009lava@gmail.com>
2026-08-24 12:57:00 +0530

    Add files via upload

---
+++ declarationandinitialization.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+char grade;
+char initial;
+char symbol;
+
+char letter = 'A';
+char digit = '5';
+char special = '2';
+char space = ' ';
+
+cout << "Letter: " << letter << endl;
+cout << "Digit: " << digit << endl;
+cout << "Special: " << special << endl;
+cout << "Space: " << space << endl;
+
+cout << "Size of char: " << sizeof(char) << "bytes" << endl;
+
+return 0;
+}
